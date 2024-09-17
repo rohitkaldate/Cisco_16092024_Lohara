830e9944a9074e35ed8c0e4247f1d1fbf9d70049
rohitkaldate <kaldaterohit26@gmail.com>
2024-09-17 19:31:10 +0530

    Qn01

---
+++ 20240916/qn01.py
@@ -0,0 +1,26 @@
+# accepting input from user:
+
+user_input= input("Enter list of words: ")
+words_list= user_input.split()
+
+print("List of words:",words_list)
+
+
+#convert list into tuple: 
+
+my_tuple= tuple(words_list)
+
+print(my_tuple)
+
+with open('qn01_data.txt','w') as data_file:
+    data_file.write(f'List:{words_list}')
+    data_file.write(f'Tuple:{my_tuple}')
+print("Data written to file")
+
+with open('qn01_data.txt','r') as data_file:
+    line_list= data_file.readline()
+    line_tuple= data_file.readline()
+    
+    print(line_list)
+    print(line_tuple)
+    
