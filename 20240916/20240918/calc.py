433ed9e91d82af94dfe9b9ffa03a0ba85188a4ab
rohitkaldate <kaldaterohit26@gmail.com>
2024-09-18 09:59:44 +0530

    Calc

--- 20240916/20240918/calc.py
+++ 20240916/20240918/calc.py
@@ -6,3 +6,12 @@ print(find_diff(second= 10, first= 20)) #10  (20-10)
 
 print(find_diff(second=10)) #-9  (1-10)
 print(find_diff())#1  (1-0)
+
+
+def change_name(names, index, name):
+    names[index] = name
+    
+names = ['rahul','modi']
+print(names)
+change_name(names,1,'modiji')
+print(names)
