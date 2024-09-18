1391e19b47c7b373c491c65d31f4b5f5501767f9
rohitkaldate <kaldaterohit26@gmail.com>
2024-09-18 14:21:25 +0530

    OOPs

--- 20240916/20240918/OOPs/map_filter_reduce.py
+++ 20240916/20240918/OOPs/map_filter_reduce.py
@@ -12,3 +12,13 @@ nums_sum = reduce(lambda s,num: s+num, nums,0)
 nums_prod = reduce(lambda p,num: p * num, nums,1)
 
 print(nums_sum,nums_prod)
+
+import sys
+nums_min = reduce(lambda m,num: min(m,num), nums,sys.maxsize)
+nums_max = reduce(lambda m,num: max(m,num),nums,-sys.maxsize)
+print(nums_min,nums_max)
+
+nums_min = reduce(min,nums)
+nums_max = reduce(max,nums)
+
+print(nums_min, nums_max)
