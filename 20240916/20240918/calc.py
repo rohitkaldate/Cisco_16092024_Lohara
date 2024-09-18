7fbaef222af747392689f099665d61a71a742800
rohitkaldate <kaldaterohit26@gmail.com>
2024-09-18 09:49:47 +0530

    Calc

---
+++ 20240916/20240918/calc.py
@@ -0,0 +1,8 @@
+def find_diff(first=1, second=0):
+    return first - second
+
+print(find_diff(20,10)) #10
+print(find_diff(second= 10, first= 20)) #10  (20-10)
+
+print(find_diff(second=10)) #-9  (1-10)
+print(find_diff())#1  (1-0)
