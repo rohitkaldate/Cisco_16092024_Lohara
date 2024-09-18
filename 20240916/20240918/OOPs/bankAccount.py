fc8b973146a5753d47f547db09be30513fd0af16
rohitkaldate <kaldaterohit26@gmail.com>
2024-09-18 14:52:12 +0530

    OOPs

---
+++ 20240916/20240918/OOPs/bankAccount.py
@@ -0,0 +1,26 @@
+class Account:
+    def __init__(self,number,name,initial_amount=1000):
+        self.__number= number
+        self.__name= name
+        self.__balance= initial_amount     
+    def __repr__(self):
+        return f'[number= {self.__number},name= {self.__name},balance= {self.__balance}]'
+    def __str__(self):
+        return self.__repr__()
+    def deposit(self,amount):
+        self.__balance += amount
+    def withdraw(self,amount):
+        self.__balance -= amount
+        
+raj_ac= Account(name= 'Raj',number= '3739111945',initial_amount= 3000)
+print(raj_ac)
+raj_ac.deposit(200000)
+print(raj_ac)
+raj_ac.deposit(10000)
+print(raj_ac)
+
+raj_ac.withdraw(50000)
+print(raj_ac)
+print(raj_ac.__dict__)
+print(raj_ac.__balance)
+
